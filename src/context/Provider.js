47303141326f48f6a20803fa3fa59638bdeaf544
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-30 18:03:38 -0300

    Teste branch

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -2,28 +2,27 @@ import React, { useState, useEffect } from 'react';
 import AppReceitaContext from './AppReceitaContext';
 import fetchFoodApi from '../services/index';
 
-function Provider ({ children }) {
-
-  const [data, setData] = useState([]);
+function Provider({ children }) {
+  const [dataFood, setDataFood] = useState([]);
 
   useEffect(() => {
-    getFood().then((data) => setData(data.meals));
+    getFood().then((data) => setDataFood(data.meals));
   }, []);
 
   const getFood = async () => {
     const data = await fetchFoodApi();
     return data;
-  }
+  };
 
   const contextValue = {
-    data,
-  }
-
+    dataFood,
+  };
+  // aloo teste
   return (
     <AppReceitaContext.Provider value={contextValue}>
       {children}
     </AppReceitaContext.Provider>
-  )
+  );
 }
 
 export default Provider;

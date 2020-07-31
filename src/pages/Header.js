3b40970cf57ad78bd91973b4435c02fa02be5fc5
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-07-31 15:37:13 -0300

    link para perfil adicionado

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -5,7 +5,13 @@ import searchIcon from '../images/searchIcon.svg';
 const Header = () => {
   return (
     <header>
-      <img data-testid="profile-top-btn" src={profileIcon} alt="profile icon" />
+      <Link to="/perfil">
+        <img
+          data-testid="profile-top-btn"
+          src={profileIcon}
+          alt="profile icon"
+        />
+      </Link>
       <h1 data-testid="page-title">"Titulo da página"</h1>
       <img data-testid="search-top-btn" src={searchIcon} alt="search icon" />
     </header>

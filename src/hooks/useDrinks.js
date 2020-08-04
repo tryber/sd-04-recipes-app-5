6ac119910c7be025d7048a8f2794215ee41bbe49
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-04 18:28:26 -0300

    Pagina de drinks added

--- src/hooks/useDrinks.js
+++ src/hooks/useDrinks.js
@@ -1,17 +1,17 @@
 import { useEffect, useContext } from 'react';
-import fetchDrinkApi from '../services/DrinkDB-API';
+import { getDrinks } from '../services/DrinkDB-API';
 import AppReceitaContext from '../context/AppReceitaContext';
 
 const useDrinks = () => {
   const { dataDrink, setDataDrink } = useContext(AppReceitaContext);
 
   const getDrink = async () => {
-    const data = await fetchDrinkApi();
+    const data = await getDrinks();
     return data;
   };
 
   useEffect(() => {
-    getDrink().then((data) => setDataDrink(data));
+    getDrink().then((drinks) => setDataDrink(drinks.drinks));
   }, []);
 
   return dataDrink;
--- src/pages/Drink.js
+++ src/pages/Drink.js
@@ -1,16 +1,34 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import AppReceitasContext from '../context/AppReceitaContext';
 import Footer from '../components/Footer';
+import useDrinks from '../hooks/useDrinks';
 
 function Drink() {
-  const { idDrink } = useContext(AppReceitasContext);
+  const drinks = useDrinks();
+
+  if (!drinks) return <p> Loading... </p>;
+
   return (
-    <div>
-      <div className="DetailsDrink">
-        <Link to={`/bebidas/${idDrink}`}>
-          <p>Each Drink</p>
-        </Link>
+    <div className="Drink">
+      <div className="container">
+        <div className="row row-cols-2">
+          {drinks.slice(0, 12).map((drink) => (
+            <div key={drink.idDrink} className="col">
+              <Link to={`/comidas/${drink.idDrink}`}>
+                <div className="card" style={{ width: '18rem' }}>
+                  <img
+                    src={drink.strDrinkThumb}
+                    className="card-img-top"
+                    alt="Meal picture"
+                  />
+                  <div className="card-body">
+                    <p className="card-text">{drink.strDrink}</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="Footer">
         <Footer />

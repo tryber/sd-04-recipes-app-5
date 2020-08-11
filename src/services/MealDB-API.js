3a0e8ac2c88bfe76a4e8e85a1897267eaa3a0ea1
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-10 22:56:31 -0300

    CC 05

--- src/services/MealDB-API.js
+++ src/services/MealDB-API.js
@@ -31,33 +31,29 @@ export const getMeal = (id) =>
 // )
 
 export const getMeals = () =>
-  fetch(
-    'https://www.themealdb.com/api/json/v1/1/search.php?s='
-  ).then((response) => response.json());
+  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+    .then((response) => response.json());
 // .then((json) =>
 //   response.ok ? Promise.resolve(json) : Promise.reject(json)
 // )
 
 export const getMealsCategories = () =>
-  fetch(
-    'https://www.themealdb.com/api/json/v1/1/list.php?c=list'
-  ).then((response) => response.json());
+  fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+    .then((response) => response.json());
 // .then((json) =>
 //   response.ok ? Promise.resolve(json) : Promise.reject(json)
 // )
 
 export const getMealsByCategory = (cat) =>
-  fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`
-  ).then((response) => response.json());
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`)
+    .then((response) => response.json());
 // .then((json) =>
 //   response.ok ? Promise.resolve(json) : Promise.reject(json)
 // )
 
 export const getMealDetailsById = (id) =>
-  fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-  ).then((response) => response.json());
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((response) => response.json());
 // .then((json) =>
 //   response.ok ? Promise.resolve(json) : Promise.reject(json)
 // )

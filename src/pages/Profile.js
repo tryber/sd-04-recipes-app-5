614ef8a234657a4f6763997f183e59cd4dd520a9
samueap <samueap@gmail.com>
2020-08-12 10:50:53 -0300

    profile done

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,18 +4,22 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export function Food() {
+  const user = JSON.parse(localStorage.user);
   return (
     <div>
       <Header pageTitle="Perfil" searchBtn={false} />
+      <p data-testid="profile-email">{user.email}</p>
       <div className={'userButtons'}>
         <Link to={'/receitas-feitas'}>
-          <span>Receitas Feitas</span>
+          <span data-testid="profile-done-btn">Receitas Feitas</span>
         </Link>
         <Link to={'/receitas-favoritas'}>
-          <span>Receitas Favoritas</span>
+          <span data-testid="profile-favorite-btn">Receitas Favoritas</span>
         </Link>
         <Link to={'/'}>
-          <span>Sair</span>
+          <span data-testid="profile-logout-btn" onClick={() => window.localStorage.clear()}>
+            Sair
+          </span>
         </Link>
       </div>
       <div className="Footer">

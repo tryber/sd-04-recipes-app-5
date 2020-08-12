90a53d1adc628aad96f747d3b61b1cc1fecffa8a
samueap <samueap@gmail.com>
2020-08-12 13:51:42 -0300

    API to list countries

--- src/pages/ExploreFoodByArea.js
+++ src/pages/ExploreFoodByArea.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+// import { getMeals, getCountryList, getCountryFilter } from '../services/MealDB-API';
 
 function ExploreFoodByArea() {
   return (
--- src/services/MealDB-API.js
+++ src/services/MealDB-API.js
@@ -24,39 +24,59 @@ export const getMealByName = (name) =>
 
 const ID_URL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 
-export const getMeal = (id) =>
-  fetch(`${ID_URL}${id}`).then((response) => response.json());
+export const getMeal = (id) => fetch(`${ID_URL}${id}`).then((response) => response.json());
 // .then((json) =>
 //   response.ok ? Promise.resolve(json) : Promise.reject(json)
 // )
 
 export const getMeals = () =>
-  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
-    .then((response) => response.json());
+  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=').then((response) =>
+    response.json(),
+  );
 // .then((json) =>
 //   response.ok ? Promise.resolve(json) : Promise.reject(json)
 // )
 
 export const getMealsCategories = () =>
-  fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
-    .then((response) => response.json());
+  fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list').then((response) =>
+    response.json(),
+  );
 // .then((json) =>
 //   response.ok ? Promise.resolve(json) : Promise.reject(json)
 // )
 
 export const getMealsByCategory = (cat) =>
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`)
-    .then((response) => response.json());
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`).then((response) =>
+    response.json(),
+  );
 // .then((json) =>
 //   response.ok ? Promise.resolve(json) : Promise.reject(json)
 // )
 
 export const getMealDetailsById = (id) =>
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-    .then((response) => response.json());
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`).then((response) =>
+    response.json(),
+  );
 // .then((json) =>
 //   response.ok ? Promise.resolve(json) : Promise.reject(json)
 // )
+
+// Para tela de origem/area
+const URL_COUNTRYLIST = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
+
+export const getCountryList = () =>
+  fetch(`${URL_COUNTRYLIST}`).then((response) =>
+    response.json(console.log('respListCountry', response))
+    // .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
+  );
+
+const URL_COUNTRYFILTER = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=';
+
+export const getCountryFilter = (country) =>
+  fetch(`${URL_COUNTRYFILTER}${country}`).then((response) =>
+    response.json(console.log('respCountryF', response))
+    // .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
+  );
 
 export const receivedSearch = (obj, resolver) => {
   const json = { meals: obj };

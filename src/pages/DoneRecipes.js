0b02c66e064a050973d00166d365f34e9eb67f74
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-12 00:52:42 -0300

    Requisito 58 done

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -2,9 +2,16 @@ import React, { useContext, useState } from 'react';
 import Header from '../components/Header';
 import AppReceitaContext from '../context/AppReceitaContext';
 import shareIcon from '../images/shareIcon.svg';
+import copy from 'clipboard-copy';
+
+const handleShare = (type, id, setIsShow) => {
+  copy(`http://localhost:3000/${type}/${id}`);
+  setIsShow(false);
+};
 
 const doneDrink = (recipe) => {
-  const {name, image, doneDate, alcoholicOrNot,} = recipe;
+  const [isShow, setIsShow] = useState(true);
+  const { id, name, image, doneDate, alcoholicOrNot } = recipe;
 
   return (
     <div>
@@ -12,16 +19,21 @@ const doneDrink = (recipe) => {
       <p>Nome: {name}</p>
       <p>{alcoholicOrNot}</p>
       <p>Data de realização: {doneDate}</p>
-      <button>
-        <img src={shareIcon} />
-      </button>
+      {isShow ? (
+        <button onClick={() => handleShare('bebidas', id, setIsShow)}>
+          <img src={shareIcon} />
+        </button>
+      ) : (
+        <p>Link copiado!</p>
+      )}
     </div>
   );
 };
 
 const doneFood = (recipe) => {
-  const {area, category, name, image, doneDate, tags} = recipe;
-  const tags = tags.slice(0,2);
+  const [isShow, setIsShow] = useState(true);
+  const { area, category, name, image, doneDate, tags } = recipe;
+  const tags = tags.slice(0, 2);
   return (
     <div className="doneFoods">
       <img src={image} alt="Foto da imagem" />
@@ -29,14 +41,18 @@ const doneFood = (recipe) => {
       <p>Categoria: {category}</p>
       <p>Area: {area}</p>
       <p>Data de realização: {doneDate}</p>
-      {tags.map((tag) => 
+      {tags.map((tag) => (
         <div>
           <p>Tag API: {tag}</p>
         </div>
-        )}
-      <button>
-        <img src={shareIcon} />
-      </button>
+      ))}
+      {isShow ? (
+        <button onClick={() => handleShare('comidas', id, setIsShow)}>
+          <img src={shareIcon} />
+        </button>
+      ) : (
+        <p>Link copiado!</p>
+      )}
     </div>
   );
 };
@@ -47,7 +63,9 @@ function DoneRecipes() {
   return (
     <div>
       <Header pageTitle="Receitas Feitas" searchBtn={false} />
-      {doneRecipes.map((recipe) => recipe.type === 'comida' ? doneFood(recipe) : doneDrink(recipe))}
+      {doneRecipes.map((recipe) =>
+        recipe.type === 'comida' ? doneFood(recipe) : doneDrink(recipe)
+      )}
     </div>
   );
 }

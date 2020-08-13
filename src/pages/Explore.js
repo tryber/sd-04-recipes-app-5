42e014ba01a9d73cb78a9e121545049954291be2
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-08-13 08:34:40 -0300

    tela de explorar passando em todos requisitos

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,14 @@ function Explorar() {
   return (
     <div>
       <Header pageTitle="Explorar" searchBtn={false} />
+      <div>
+        <Link to="/explorar/comidas">
+          <button data-testid="explore-food">Explorar Comidas</button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button data-testid="explore-drinks">Explorar Bebidas</button>
+        </Link>
+      </div>
       <div className="Footer">
         <Footer />
       </div>

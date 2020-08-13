a77c10a0704da45ac781e8872f0835686122515c
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-08-13 09:41:13 -0300

    requisitos tela de explorar comidas ou bebidas ok

--- src/services/DrinkDB-API.js
+++ src/services/DrinkDB-API.js
@@ -1,33 +1,41 @@
 const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
 
-export const getDrinkByLetter = (letter) => fetch(`${BASE_URL}${letter}`).then((response) => response
-  .json()
-  .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));
+export const getDrinkByLetter = (letter) =>
+  fetch(`${BASE_URL}${letter}`).then((response) =>
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
+  );
 
 const ING_URL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
 
-export const getDrinkByIngredients = (ingredient) => fetch(`${ING_URL}${ingredient}`).then((response) => response
-  .json()
-  .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));
+export const getDrinkByIngredients = (ingredient) =>
+  fetch(`${ING_URL}${ingredient}`).then((response) =>
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
+  );
 
 const NAME_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const getDrinkByName = (name) => fetch(`${NAME_URL}${name}`).then((response) => response
-  .json()
-  .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));
+export const getDrinkByName = (name) =>
+  fetch(`${NAME_URL}${name}`).then((response) =>
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
+  );
 
-export const getDrink = (id) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
-  .then((resp) => resp.json());
+export const getDrink = (id) =>
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`).then((resp) =>
+    resp.json(),
+  );
 
 export const getDrinks = () =>
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
-    .then((resp) => resp.json());
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=').then((resp) => resp.json());
 
 export const getDrinksCategories = () =>
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list').then((response) => response
-    .json()
-    .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list').then((response) =>
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
+  );
 
-export const getDrinksByCategory = (cat) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${cat}`).then((response) => response
-  .json()
-  .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));
+export const getDrinksByCategory = (cat) =>
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${cat}`).then((response) =>
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
+  );
+
+export const getRandomDrink = () =>
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php').then((data) => data.json());
